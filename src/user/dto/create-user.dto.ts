84b8c75db1af7c1eb06b2ddbeff60104aee1f6ab
rodrigocitadin <citadinrodrigo@gmail.com>
2023-03-13 23:14:58 -0300

    updating dtos

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,12 +1,14 @@
-import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsStrongPassword, Length } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
   readonly full_name: string;
 
   @IsNotEmpty()
+  @Length(11)
   readonly cpf: string;
 
+  @IsNotEmpty()
   @IsEmail()
   readonly email: string;
 
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,6 +1,18 @@
+import { IsEmail, IsOptional, IsStrongPassword, Length } from 'class-validator';
+
 export class UpdateUserDto {
+  @IsOptional()
   readonly full_name: string;
+
+  @IsOptional()
+  @Length(11)
   readonly cpf: string;
+
+  @IsOptional()
+  @IsEmail()
   readonly email: string;
+
+  @IsOptional()
+  @IsStrongPassword()
   readonly password: string;
 }
